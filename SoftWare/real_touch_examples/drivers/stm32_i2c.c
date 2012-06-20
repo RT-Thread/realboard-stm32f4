a5b1b1ea37f04156d6283b4fe25936378c2b6129
unknown <luohui@luohui-PC.(none)>
2012-06-20 23:33:41 +0800

    update STM32 I2C driver(support I2C controller)

--- SoftWare/real_touch_examples/drivers/stm32_i2c.c
+++ SoftWare/real_touch_examples/drivers/stm32_i2c.c
@@ -71,6 +71,8 @@ static const struct rt_i2c_bit_ops bit_ops =
 };
 
 #else
+
+#if 0
 static rt_err_t stm32_i2c_configure(struct rt_i2c_bus_device * device,
                                     struct rt_i2c_configuration* configuration)
 {
@@ -192,16 +194,132 @@ static rt_size_t stm32_i2c_write(struct rt_i2c_bus_device * device,
 
 	return 0;
 }
+#endif
 
-struct rt_i2c_bus_device stm32_i2c1;
-const struct rt_i2c_ops stm32_i2c_ops =
+static rt_size_t stm32_i2c_recv_bytes(I2C_TypeDef* I2Cx, struct rt_i2c_msg *msg)
 {
-    stm32_i2c_configure,
-    stm32_i2c_start,
-    stm32_i2c_stop,
-    stm32_i2c_read,
-    stm32_i2c_write,
+    rt_size_t bytes = 0;
+
+    while (msg->len--)
+    {
+        while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED));
+
+        msg->buf[bytes++] = (rt_uint8_t)I2Cx->DR;
+    }
+
+    return bytes;
+}
+
+
+static rt_size_t stm32_i2c_send_bytes(I2C_TypeDef* I2Cx, struct rt_i2c_msg *msg)
+{
+    rt_size_t bytes = 0;
+
+    while (msg->len--)
+    {
+        I2Cx->DR = msg->buf[bytes++];
+        while(! I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+    }
+
+    return bytes;
+}
+
+
+static void I2C_SendAddress(I2C_TypeDef* I2Cx, struct rt_i2c_msg *msg)
+{
+  rt_uint16_t addr;
+  rt_uint16_t flags = msg->flags;
+  /* Check the parameters */
+  assert_param(IS_I2C_ALL_PERIPH(I2Cx));
+  /* Test on the direction to set/reset the read/write bit */
+  addr = msg->addr << 1;
+  if (flags & RT_I2C_RD)
+  {
+    /* Set the address bit0 for read */
+    addr |= 1;
+  }
+  /* Send the address */
+  I2Cx->DR = addr;
+}
+
+
+static rt_size_t stm32_i2c_xfer(struct rt_i2c_bus_device *bus,
+                              struct rt_i2c_msg msgs[], rt_uint32_t num)
+{
+    struct rt_i2c_msg *msg;
+    rt_int32_t i, ret;
+    rt_uint16_t ignore_nack;
+
+    I2C_GenerateSTART(I2C1, ENABLE);
+    /* Test on EV5 and clear it */
+    while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+
+    for (i = 0; i < num; i++)
+    {
+        msg = &msgs[i];
+        ignore_nack = msg->flags & RT_I2C_IGNORE_NACK;
+        if (!(msg->flags & RT_I2C_NO_START))
+        {
+            if (i)
+            {
+                I2C_GenerateSTART(I2C1, ENABLE);
+            }
+            I2C_SendAddress(I2C1, msg);
+            if (msg->flags & RT_I2C_RD)
+                while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+            else
+                while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+            
+        }
+        if (msg->flags & RT_I2C_RD)
+        {
+            if (!(msg->flags & RT_I2C_NO_READ_ACK))
+                I2C_AcknowledgeConfig(I2C1,ENABLE);
+            else
+                I2C_AcknowledgeConfig(I2C1,DISABLE);
+            ret = stm32_i2c_recv_bytes(I2C1, msg);
+            if (ret >= 1)
+                i2c_dbg("read %d byte%s\n",
+                        ret, ret == 1 ? "" : "s");
+            if (ret < msg->len)
+            {
+                if (ret >= 0)
+                    ret = -RT_EIO;
+                goto out;
+            }
+        }
+        else
+        {
+        	ret = stm32_i2c_send_bytes(I2C1, msg);
+            if (ret >= 1)
+                i2c_dbg("write %d byte%s\n",
+                        ret, ret == 1 ? "" : "s");
+            if (ret < msg->len)
+            {
+                if (ret >= 0)
+                    ret = -RT_ERROR;
+                goto out;
+            }
+        }
+    }
+    ret = i;
+
+out:
+    i2c_dbg("send stop condition\n");
+    I2C_GenerateSTOP(I2C1, ENABLE);
+
+    return ret;
+}
+
+
+static const struct rt_i2c_bus_device_ops i2c1_ops = {
+    stm32_i2c_xfer,
+    RT_NULL,
+    RT_NULL
 };
+
+static struct rt_i2c_bus_device stm32_i2c1;
+
 #endif
 
 void rt_hw_i2c_init(void)
@@ -275,6 +393,10 @@ void rt_hw_i2c_init(void)
     /* sEE_I2C Peripheral Enable */
     I2C_Cmd(I2C1, ENABLE);
 
-    rt_i2c_bus_register(&stm32_i2c1, "i2c1", &stm32_i2c_ops);
+    rt_memset((void *)&stm32_i2c1, 0, sizeof(struct rt_i2c_bus_device));
+    stm32_i2c1.ops = &i2c1_ops;
+
+    rt_i2c_bus_device_register(&stm32_i2c1, "i2c1");
+
 #endif
 }
