266701c55acb7d09f5a076f7320cc8e723da1db1
yiyue.fang <dzzxzz@gmail.com>
2012-08-30 15:12:32 +0800

    correct filename to application.c
    add some comments

--- software/examples/examples/1_kernel_thread_yield/applications/application.c
+++ software/examples/examples/1_kernel_thread_yield/applications/application.c
@@ -1,14 +1,28 @@
+/*
+ * File      : application.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006 - 2012, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
 #include <rtthread.h>
 
 /* 指向线程控制块的指针 */
 static rt_thread_t tid1 = RT_NULL;
 static rt_thread_t tid2 = RT_NULL;
+
 /* 线程1入口 */
-static void thread1_entry(void* parameter)
+static void thread1_entry(void *parameter)
 {
     rt_uint32_t count = 0;
 
-    for(;count<5;count++)
+    for (; count < 5; count ++)
     {
         /* 打印线程1的输出 */
         rt_kprintf("thread1: count = %d\n", count);
@@ -18,28 +32,25 @@ static void thread1_entry(void* parameter)
 }
 
 /* 线程2入口 */
-static void thread2_entry(void* parameter)  
+static void thread2_entry(void *parameter)
 {
     rt_uint32_t count = 0;
 
-    for(;count<5;count++)
+    for (; count < 5; count ++)
     {
         /* 打印线程2的输出 */
         rt_thread_yield();
         rt_kprintf("thread2: count = %d\n", count);
     }
-    
 }
 
-
-int rt_application_init()
+int rt_application_init(void)
 {
     tid1 = rt_thread_create("thread",
         thread1_entry, RT_NULL, /* 线程入口是thread1_entry, 入口参数是RT_NULL */
         512, 6, 100);
     if (tid1 != RT_NULL)
         rt_thread_startup(tid1);
-
 
     /* 创建线程2 */
     tid2 = rt_thread_create("thread",
