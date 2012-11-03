cc5d612dfd19b15a9b01cdeec0a6d532ee34f738
Romeo <xpxyr@vip.qq.com>
2012-11-03 15:13:31 +0800

    update snake core.

--- software/programs/snake/snake.c
+++ software/programs/snake/snake.c
@@ -1,90 +1,253 @@
-/* snake.c */
-/* author:master@aozima.cn */
-/* update:Mar 5 2009 10:01 */
-
+#include <string.h>
+#include <stdlib.h>
+#include <rtthread.h>
 #include "snake.h"
 
-snake_struct snake;
+rt_list_t snake_head;
+SNAKE_DIR prevdir;
 
-void snake_init(void) /* 蛇初始化 */
+static SNAKE_DIR dir_adjust(SNAKE_DIR dir)
 {
-    unsigned char i;
-    snake.flag = 0x00;
+    if (    (SNAKE_DIR_UP == prevdir && SNAKE_DIR_DOWN != dir)
+            ||  (SNAKE_DIR_DOWN == prevdir && SNAKE_DIR_UP != dir)
+            ||  (SNAKE_DIR_LEFT == prevdir && SNAKE_DIR_RIGHT != dir)
+            ||  (SNAKE_DIR_RIGHT == prevdir && SNAKE_DIR_LEFT != dir)
+       )
+    {
+        prevdir = dir;
+    }
+    else
+    {
+        rt_kprintf("dirction change error\n\r");
+    }
 
-    snake.x[0] = 5;//(snake_romm_size_widht / 2); /* 设定第一次豆子出现的位置 */
-    snake.y[0] = 5;//(snake_room_size_hight / 2); /* 默认为最中心             */
-
-    snake.length_now = snake_length_init;     /* 初始化当前蛇长           */
-
-    for(i=1;i<snake_length_init+1;i++){snake.y[i] = snake_length_init-i;}
-    for(i=snake_room_size_hight;i>snake_length_init;i--){snake.y[i+1] = snake_room_size_hight;}
-
-    for(i=0;i<snake_length_init;i++){snake.x[i+1] = 0;}
-    for(;i<snake_room_size_widht;i++){snake.x[i+1] = snake_room_size_widht;}
-
-    snake.addx = 0;
-    snake.addy = 0;
+    return prevdir;
 }
 
-void snake_run_step(void) /* 蛇运行一步 */
+static void across_XY(point_t *node, const map_t *map)
 {
-    unsigned char i;
+    RT_ASSERT(node != RT_NULL && map != RT_NULL);
 
-    if((snake.addx ==0) & (snake.addy == 0) ){return;}
+    // 如果长度超出当前边框则可以穿越墙到对面
+    node->x = (node->x + map->width) % map->width;
+    node->y = (node->y + map->height) % map->height;
+}
 
-    /* 判断是否吃东西 */
-    if( (snake.x[0] == snake.x[1] + snake.addx) && (snake.y[0] == snake.y[1] + snake.addy) )
+static SYS_STE node_update(snake_t *tail, const point_t *node, map_t *map)
+{
+    SYS_STE ret;
+    point_t *pos;
+
+    RT_ASSERT(tail != RT_NULL && node != RT_NULL && map != RT_NULL);
+
+    pos = map->snake_flush;
+    pos[0].x = pos[0].y = -1;
+    pos[1].x = pos[1].y = -1;
+
+    ret = (SYS_STE)map->range[node->y * map->width + node->x];
+
+    if (FOOD == map->range[node->y * map->width + node->x])
     {
-        snake.length_now++;                                          /* 蛇长+1             */
-        if(snake.length_now == snake_length_max)                     /* 判断是否通关       */
+        // 吃一个食物增加一个节点
+        snake_t *new = (snake_t*)rt_malloc(sizeof(snake_t));
+        if (!new)
+            return NORMAL;
+
+        pos[0] = *node;
+        new->body = *node;
+        rt_list_insert_after(&snake_head, &new->list);
+    }
+    else if (NORMAL == map->range[node->y * map->width + node->x])
+    {
+        // 将尾巴修改后拿到头部，其他不变
+        rt_list_remove(&tail->list);
+        map->range[tail->body.y * map->width + tail->body.x] = NORMAL;
+
+        pos[0] = *node;
+        pos[1] = tail->body;
+
+        tail->body = *node;
+        rt_list_insert_after(&snake_head, &tail->list);
+    }
+
+    map->range[node->y * map->width + node->x] = OVER;
+    return ret;
+}
+
+
+map_t* map_init(rt_uint32_t width, rt_uint32_t heigth)
+{
+    map_t *map = rt_malloc(sizeof(map_t));
+
+    if (map != RT_NULL)
+    {
+        map->range = rt_malloc(heigth * width);
+
+        if (!map->range)
         {
-            snake.flag |= (1<<7);                                    /* 设置通关标致       */
-            snake.addx = 0;snake.addy = 0;                           /* 如果通关就停止运行 */
+            rt_free(map);
+            map = RT_NULL;
         }
         else
         {
-            snake.x[0] = snake.x[snake.length_now - 1];
-            snake.y[0] = snake.y[snake.length_now - 1];
+            map->width = width;
+            map->height = heigth;
+            memset(map->range, NORMAL, heigth * width);
         }
     }
-    for(i=snake.length_now;i>1;i--)
-    {
-        snake.x[i] = snake.x[i-1];
-        snake.y[i] = snake.y[i-1];
-    }
-    snake.x[1] = snake.x[2] + snake.addx;
-    snake.y[1] = snake.y[2] + snake.addy;
 
-
-    /* 判断撞墙 */
-    if( (snake.x[1] > (snake_room_size_widht - 1)) || (snake.y[1] > (snake_room_size_hight - 1)) )
-    {
-        snake.addx = 0;snake.addy = 0;
-        snake.flag |= (1<<0);
-    }
-
-    /* 判断是否撞自己 */
-    for(i=2;i<snake.length_now;i++)
-    {
-        if( (snake.x[1] == snake.x[i]) && (snake.y[1] == snake.y[i]) )
-        {
-            snake.addx = 0;snake.addy = 0;
-            snake.flag |= (1<<1);
-        }
-    }
+    return map;
 }
 
-void snake_key(unsigned char key)
+// 构造一条指定长度的蛇在指定点
+rt_bool_t snake_init(const point_t *start, const int length, const SNAKE_DIR dir, map_t *map)
 {
-    if(snake.flag){return;}
+    rt_int32_t i;
+    rt_int32_t inc_x, inc_y;
+    point_t old = *start;
 
-    switch(key)
+    rt_list_init(&snake_head);
+
+    if (dir == SNAKE_DIR_UP || dir == SNAKE_DIR_DOWN)
     {
-        case 1 : if( (snake.y[1] + 1) != snake.y[2] ){ snake.addx = 0;snake.addy =  1;}break; /* up    */
-        case 2 : if( (snake.y[1] - 1) != snake.y[2] ){ snake.addx = 0;snake.addy = -1;}break; /* down  */
-        case 3 : if( (snake.x[1] - 1) != snake.x[2] ){ snake.addy = 0;snake.addx = -1;}break; /* left  */
-        case 4 : if( (snake.x[1] + 1) != snake.x[2] ){ snake.addy = 0;snake.addx =  1;}break; /* right */
-        default:break;
+        if (map->height <= length)
+            return RT_FALSE;
+
+        inc_x = 0;
+        inc_y = dir == SNAKE_DIR_DOWN ? 1 : -1;  // 反向延长身子，头部在指定位置
+        old.y -= inc_y;
+    }
+    else
+    {
+        if (map->width <= length)
+            return RT_FALSE;
+
+        inc_y = 0;
+        inc_x = dir == SNAKE_DIR_RIGHT ? -1 : 1;
+        old.x -= inc_x;
+    }
+
+    for (i=0; i<length; i++)
+    {
+        snake_t *new = (snake_t*)rt_malloc(sizeof(snake_t));
+        if (!new)
+            return RT_FALSE;
+
+        new->body.y = inc_y + old.y;
+        new->body.x = inc_x + old.x;
+
+        // 如果长度超出当前边框则可以穿越墙到对面
+        across_XY(&new->body, map);
+
+        map->range[new->body.y * map->width + new->body.x] = OVER;
+
+        old = new->body;
+        rt_list_insert_before(&snake_head, &new->list);
+    }
+
+    prevdir = dir;
+
+    return RT_TRUE;
+}
+
+// 构造出食物
+rt_bool_t food_init(map_t *map, rt_uint32_t max_num)
+{
+    point_t food;
+
+#ifndef FOOD_TIMEOUT
+#define FOOD_TIMEOUT    10
+#endif
+
+    rt_uint32_t timeout, num;
+
+    num = 0;
+    timeout = rt_tick_get();
+    srand(rand());
+
+    map->food_flush[0].x = map->food_flush[0].y = -1;
+
+    do
+    {
+        food.x = rand() % map->width;
+        food.y = rand() % map->height;
+
+        if (map->range[food.y * map->width + food.x] == NORMAL)
+        {
+            map->food_flush[0] = food;
+            map->range[food.y * map->width + food.x] = FOOD;
+            num++;
+        }
+    }
+    while (num < max_num && rt_tick_get() - timeout < FOOD_TIMEOUT);
+
+    return num;
+}
+
+void map_deinit(map_t *map)
+{
+    if (map)
+    {
+        if (map->range)
+        {
+            rt_free(map->range);
+            map->range = RT_NULL;
+        }
+        rt_free(map);
     }
 }
-/* end file */
+
+void snake_deinit(void)
+{
+    snake_t *node;
+
+    while (!rt_list_isempty(&snake_head))
+    {
+        node = rt_list_entry(snake_head.prev, snake_t, list);
+        rt_list_remove(&node->list);
+        rt_free(node);
+    }
+}
+
+void food_deinit(void)
+{
+
+}
+
+SYS_STE snake_step(SNAKE_DIR dir, map_t *map)
+{
+    snake_t *tail, *head;
+    point_t node;
+
+    dir = dir_adjust(dir);
+
+    // 取出头尾两个节点，其他节点不需要改变
+    tail = rt_list_entry(snake_head.prev, snake_t, list);
+    head = rt_list_entry(snake_head.next, snake_t, list);
+
+    node = head->body;
+
+    // 构造一个新的蛇头坐标
+    switch (dir)
+    {
+    case SNAKE_DIR_UP:
+    case SNAKE_DIR_DOWN:
+        node.y = head->body.y + (dir == SNAKE_DIR_DOWN ? -1 : 1);
+        break;
+    case SNAKE_DIR_LEFT:
+    case SNAKE_DIR_RIGHT:
+        node.x = head->body.x + (dir == SNAKE_DIR_RIGHT ? 1 : -1);
+        break;
+    }
+    across_XY(&node, map);
+
+    return node_update(tail, &node, map);
+}
+
+rt_bool_t snake_restart(const point_t *start, const int length, const SNAKE_DIR dir, map_t *map)
+{
+    snake_deinit();
+    memset(map->range, NORMAL, map->width * map->height);
+
+    return snake_init(start, length, dir, map);
+}
--- software/programs/snake/snake.h
+++ software/programs/snake/snake.h
@@ -1,29 +1,68 @@
-/* snake.h */
-/* author:master@aozima.cn */
-/* update:Mar 5 2009 10:01 */
+#ifndef _SNAKE_H_
+#define _SNAKE_H_
 
-#ifndef SNAKE_H_INCLUDED
-#define SNAKE_H_INCLUDED
+#include <rtthread.h>
 
 #define snake_length_max                  20  //最大蛇长
 #define snake_length_init                 3   //初始化蛇长
 #define snake_room_size_hight             8   //房子高 0-255 建议>8
 #define snake_room_size_widht             8   //房子宽 0-255 建议>8
 
+#define snake_init_pointx               1
+#define snake_init_pointy               2
+
 typedef struct
 {
-    unsigned char x[snake_length_max+1];       //蛇身X坐标值 下标为蛇身每一节的坐标
-    unsigned char y[snake_length_max+1];       //同上,下标0为豆子的值
-    unsigned char length_now;                  /* 当前蛇长 */
-    signed   char addx;
-    signed   char addy;
-    unsigned char flag;                        //蛇当前标致
-}snake_struct;
+    rt_int32_t x, y;
+}point_t;
 
-extern snake_struct snake;
+typedef struct
+{
+    rt_int32_t width;       // max x
+    rt_int32_t height;      // max y
+    rt_uint8_t *range;      // map, map->range[y * map->width + x]
+    point_t snake_flush[2];
+    point_t food_flush[1];
+}map_t;
 
-void snake_init(void);                          /* init snake */
-void snake_run_step(void);                      /* run a step */
-void snake_key(unsigned char key);              /*            */
+typedef enum
+{
+    SNAKE_DIR_UP,
+    SNAKE_DIR_DOWN,
+    SNAKE_DIR_LEFT,
+    SNAKE_DIR_RIGHT
+}SNAKE_DIR;
 
-#endif // SNAKE_H_INCLUDED
+typedef enum
+{
+    FOOD,       // 吃到水果
+    OVER,       // 咬到自身
+    NORMAL      // 正常行动
+}SYS_STE;
+
+typedef struct
+{
+    point_t body;
+    rt_list_t list;
+}snake_t;
+
+// 构造一个地图
+map_t* map_init(rt_uint32_t width, rt_uint32_t heigth);
+
+// 构造一条指定长度的蛇在指定点
+rt_bool_t snake_init(const point_t *start, const int length, const SNAKE_DIR dir, map_t *map);
+
+// 构造出食物
+rt_bool_t food_init(map_t *map, rt_uint32_t max_num);
+
+void map_deinit(map_t *map);
+
+void snake_deinit(void);
+
+void food_deinit(void);
+
+SYS_STE snake_step(SNAKE_DIR dir, map_t *map);
+
+rt_bool_t snake_restart(const point_t *start, const int length, const SNAKE_DIR dir, map_t *map);
+
+#endif
