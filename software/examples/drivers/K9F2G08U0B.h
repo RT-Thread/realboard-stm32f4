ee3f1b6863d5a834524debd1848cbb0552c566eb
aozima <wuyangyong@gmail.com>
2012-11-24 20:55:04 +0800

    add nand driver.

---
+++ software/examples/drivers/K9F2G08U0B.h
@@ -0,0 +1,26 @@
+/*
+ * File      : K9F2G08U0X.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2009 RT-Thread Develop Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2012-01-05     Bernard      first implementation
+ * 2012-01-05     aozima       first implementation
+ */
+
+#ifndef K9F2G08U0X_H_INCLUDED
+#define K9F2G08U0X_H_INCLUDED
+
+/* nandflash confg */
+#define PAGES_PER_BLOCK         64
+#define PAGE_DATA_SIZE          2048
+#define PAGE_OOB_SIZE           64
+#define NAND_MARK_SPARE_OFFSET  4
+
+
+#endif // K9F2G08U0X_H_INCLUDED
